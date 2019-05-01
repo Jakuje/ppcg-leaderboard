fb9d732db0cb6395fb3cb41779dccf105a8f47f7
xMikee1 <33165132+xMikee1@users.noreply.github.com>
2019-05-01 18:33:16 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,7 @@ try {
   var QUESTION_ID = /id=([0-9]+)/.exec(window.location.href)[1];
 } catch (e) {
   document.getElementById("disable-no-id").style.display = "none";
-  document.body.innerHTML += "<p>Whoops! You didn't specify an ID.</p>";
+  document.body.innerHTML += "<h3>Whoops! You didn't specify an ID.</h3>";
 }
 
 var api_request = new XMLHttpRequest(),
